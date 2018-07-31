4fb5d5814fd77623413cd30e99ddd2c1ef2f0104
Jack Lye <catncradle@gmail.com>
2018-07-31 11:09:07 -0400

    working on cards

--- src/components/tours.js
+++ src/components/tours.js
@@ -6,7 +6,12 @@ const Tours = () => (
       <h2 className="heading-secondary">Super stars!</h2>
     </div>
     <div className="row">
-      <div className="col-1-of-3">Col 1 of 3</div>
+      <div className="col-1-of-3">
+        <div className="card">
+          <div className="card-side card-side-front">front</div>
+          <div className="card-side card-side-back card-side-back-1">back</div>
+        </div>
+      </div>
       <div className="col-1-of-3">Col 1 of 3</div>
       <div className="col-1-of-3">Col 1 of 3</div>
     </div>
